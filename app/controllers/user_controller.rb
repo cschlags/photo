07f9f629ff8214edc282f30d84a39d4352be1215
Christina Leuci <christinaleuci@gmail.com>
2015-05-15 17:01:04 -0400

    fixed photo upload

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,12 @@
 class UserController < ApplicationController
+  def create
+    @user = User.create( user_params )
+  end
+  def index
+    User.all
+  end
+  private
+  def user_params
+    params.require(:user).permit(:avatar)
+  end
 end
